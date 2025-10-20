df29ae85c65dc6401d7f00d41fd5c56d5c1b2253
amalkoc <armin.malkoc@insparx.com>
2025-10-20 16:06:15 +0200

    extracted logic into groovy classes

--- vars/buildPush.groovy
+++ vars/buildPush.groovy
@@ -1,9 +1,13 @@
 #!/usr/bin/env groovy
 
+import com.example.Docker
+
 def call() {
-    withCredentials([usernamePassword(credentialsId: 'docker-full-pip', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh "docker build -t 192.168.0.112:3031/docker-full-pipeline:twn-exc-app-${IMAGE_NAME} ."
-        sh 'echo $PASS | docker login -u $USER --password-stdin 192.168.0.112:3031'
-        sh "docker push 192.168.0.112:3031/docker-full-pipeline:twn-exc-app-${IMAGE_NAME}"
+    //withCredentials([usernamePassword(credentialsId: 'docker-full-pip', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+    //    sh "docker build -t 192.168.0.112:3031/docker-full-pipeline:twn-exc-app-${IMAGE_NAME} ."
+    //    sh 'echo $PASS | docker login -u $USER --password-stdin 192.168.0.112:3031'
+    //    sh "docker push 192.168.0.112:3031/docker-full-pipeline:twn-exc-app-${IMAGE_NAME}"
+
     }
+    return new Docker(this).buildDockerImage()
 }
