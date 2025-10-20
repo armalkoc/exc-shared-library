ef127c1a8088d622af88372ef55060ea2bfb9ec5
amalkoc <armin.malkoc@insparx.com>
2025-10-20 16:14:40 +0200

    added source in front of sh commands II

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -13,9 +13,9 @@ class Docker implements Serializable {
 
     def buildDockerImage() {
         source.withCredentials([source.usernamePassword(credentialsId: 'docker-full-pip', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-            source.sh "docker build -t 192.168.0.112:3031/docker-full-pipeline:twn-exc-app-${IMAGE_NAME} ."
+            source.sh "docker build -t 192.168.0.112:3031/docker-full-pipeline:twn-exc-app-${source.IMAGE_NAME} ."
             source.sh "echo '${source.PASS}' | docker login -u '${source.USER}' --password-stdin 192.168.0.112:3031"
-            source.sh "docker push 192.168.0.112:3031/docker-full-pipeline:twn-exc-app-${IMAGE_NAME}"
+            source.sh "docker push 192.168.0.112:3031/docker-full-pipeline:twn-exc-app-${source.IMAGE_NAME}"
         }
     }
 }
