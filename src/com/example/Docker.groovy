98aaa022c1ace8554a544cde42acfafe008e6e1f
amalkoc <armin.malkoc@insparx.com>
2025-10-31 07:59:36 +0100

    changed repo credentials

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -12,7 +12,7 @@ class Docker implements Serializable {
     }
 
     def buildDockerImage() {
-        source.withCredentials([source.usernamePassword(credentialsId: 'docker-full-pip', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        source.withCredentials([source.usernamePassword(credentialsId: 'docker-hub-private-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
             source.sh "docker build -t amalkoc/twn-demo-app:twn-exc-aws-app-${source.IMAGE_NAME} ."
             source.sh "echo '${source.PASS}' | docker login -u '${source.USER}' --password-stdin"
             source.sh "docker push amalkoc/twn-demo-app:twn-exc-aws-app-${source.IMAGE_NAME}"
