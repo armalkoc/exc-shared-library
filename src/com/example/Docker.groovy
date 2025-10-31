1790095312da185c165c2f5f5563e0ba04627e12
amalkoc <armin.malkoc@insparx.com>
2025-10-31 10:02:19 +0100

    editet repo

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -13,9 +13,9 @@ class Docker implements Serializable {
 
     def buildDockerImage() {
         source.withCredentials([source.usernamePassword(credentialsId: 'docker-hub-private-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-            source.sh "docker build -t amalkoc/twn-demo-app:twn-exc-aws-app-${source.IMAGE_NAME} ."
+            source.sh "docker build -t ${source.IMAGE_NAME} ."
             source.sh "echo '${source.PASS}' | docker login -u '${source.USER}' --password-stdin"
-            source.sh "docker push amalkoc/twn-demo-app:twn-exc-aws-app-${source.IMAGE_NAME}"
+            source.sh "docker push ${source.IMAGE_NAME}"
         }
     }
 }
